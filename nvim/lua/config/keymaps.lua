d997464f1d65ade35f894776e66edf31bc8e0f64
Mukul Pathania <mukulpathania96@gmail.com>
2023-05-21 20:06:53 +0530

    Setup escape with fast jk in vim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+
+vim.keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true, desc = "<ESC>" })
+vim.keymap.set("v", "jk", "<ESC>", { noremap = true, silent = true, desc = "<ESC>" })
+vim.keymap.set("x", "jk", "<ESC>", { noremap = true, silent = true, desc = "<ESC>" })
